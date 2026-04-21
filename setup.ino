b922c1d50d1f6cb67689d819c88189d95612e577
Yiying <142329093+yiying909@users.noreply.github.com>
2026-04-21 12:22:50 -0400

    first commit

---
+++ setup.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <SSD1306Wire.h>
+// SSD1306Wire lcd(0x3c, SDA, SCL);
+
+#define BUTTON1 34;
+#define BUTTON2 0;
+#define BUTTON3 35;
+
+void setup()
+{
+}
+
+void loop()
+{
+}
