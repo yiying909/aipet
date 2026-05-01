8eb6a0347e48713141ab7a372a89859823333158
Sunny <sunnyxie29@gmail.com>
2026-05-01 00:57:13 -0400

    implemented fetchTodo

--- src/fetchTodo.js
+++
@@ -1,7 +0,0 @@
-// fetch todo or calendar from app
-// can use google calendar api, see link below
-// https://developers.google.com/workspace/calendar
-
-export async function fetchDate() {
-
-}
---
+++ src/fetchTodo.py
@@ -0,0 +1,40 @@
+#fetch todo or calendar from app
+ #can use google calendar api, see link below
+#https://developers.google.com/workspace/calendar
+#export async function fetchDate() {
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/tasks.readonly']
+
+def fetch_google_tasks():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES
+    )
+    creds = flow.run_local_server(port=0)
+
+    service = build('tasks', 'v1', credentials=creds)
+
+    result = []
+    tasklists = service.tasklists().list().execute()
+
+    for tasklist in tasklists.get('items', []):
+        list_name = tasklist['title']
+        list_id = tasklist['id']
+
+        tasks = service.tasks().list(tasklist=list_id).execute()
+
+        for task in tasks.get('items', []):
+            result.append({
+                "list": list_name,
+                "title": task.get("title"),
+                "status": task.get("status"),
+            })
+
+    return result
+
+
+if __name__ == "__main__":
+    tasks = fetch_google_tasks()
+    for t in tasks:
+        print(t)
